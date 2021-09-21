e17f3f994e367d93176e0726416276747f1cf626
Artem Pavlov <pavlov.tema@gmail.com>
2021-09-21 23:27:26 +0500

    Spring framework installed. Main storyboard done.

---
+++ AnimationApp/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  AnimationApp
+//
+//  Created by iMac on 21.09.2021.
+//
+
+import Foundation
--- AnimationApp/ViewControllers/ViewController.swift
+++ AnimationApp/ViewControllers/ViewController.swift
@@ -5,15 +5,20 @@
 //  Created by iMac on 21.09.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animationsView: SpringView!
+    @IBOutlet var animationButton: SpringButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func runAnimationButton(_ sender: SpringButton) {
+    }
+    
 }
 
